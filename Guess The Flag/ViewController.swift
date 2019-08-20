7c2b5381ef18ee340d914241190e608f6fe5b0e7
Somesh Arora <somesh.arora@mavs.uta.edu>
2019-08-19 20:16:32 -0500

    Initial Commit

---
+++ Guess The Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Somesh Arora on 8/19/19.
+//  Copyright © 2019 Somesh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
